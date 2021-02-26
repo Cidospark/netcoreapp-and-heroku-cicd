0b653dd6660ba881fbd2437eb5c77735b7bba995
cidospark <cidospark27@gmail.com>
2021-02-26 06:03:09 +0100

    Initial setup

---
+++ UMS/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UMS.Models;
+
+namespace UMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        public IActionResult Index()
+        {
+            //if((Request.ContentType) == "application/json")
+            //{
+            //    return Json(_employeeRepository.GetEmployee(1).Name);
+            //}
+
+            //return View(_employeeRepository.GetEmployee(1));
+            return View(_employeeRepository.GetEmployees());
+        }
+
+
+        //public IActionResult Index()
+        //{
+        //    return View(_employeeRepository.GetEmployees());
+        //}
+    }
+}
---
+++ UMS/Models/EmployeeRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UMS.Models
+{
+    
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+        public EmployeeRepository()
+        {
+            _employeeList = new List<Employee>
+            {
+                new Employee{ Id = 1, Name = "Mary", Department="HR", Email = "mary@sample.com" },
+                new Employee{ Id = 2, Name = "John", Department="IT", Email = "john@sample.com" },
+                new Employee{ Id = 3, Name = "Sam", Department="IT", Email = "sam@sample.com" }
+            };
+        }
+        public Employee GetEmployee(int id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == id);
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return _employeeList;
+        }
+    }
+}
