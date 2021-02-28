199bd9739d36b73ec5e304de25973ba767ee8e1b
cidospark <cidospark27@gmail.com>
2021-02-28 18:44:43 +0100

    Added context class and extended identity

--- UMS/Models/AppUser.cs
+++ UMS/Models/AppUser.cs
@@ -1,17 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace UMS.Models
 {
-    public class AppUser
+    public class AppUser : IdentityUser
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }
         public string Gender { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
         public List<Photo> Photos { get; set; }
--- UMS/Startup.cs
+++ UMS/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using UMS.Data;
 using UMS.Models;
 
 namespace UMS
@@ -23,6 +24,7 @@ namespace UMS
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<UMSDbContext>(option => option.);
             services.AddMvc();
         }
 
