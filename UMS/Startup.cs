0010e6c231033fc45aebafa2815fbba9afb0c9a3
cidospark <cidospark27@gmail.com>
2021-02-28 22:20:07 +0100

    Preseeded some users

--- UMS/Startup.cs
+++ UMS/Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using UMS.Data;
@@ -24,12 +26,13 @@ namespace UMS
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContextPool<UMSDbContext>(option => option.);
+            services.AddDbContextPool<UMSDbContext>(option => option.UseSqlite(_config.GetConnectionString("default")));
+            services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<UMSDbContext>();
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, UMSDbContext ctx, RoleManager<IdentityRole> roleMgr, UserManager<AppUser> userMgr)
         {
             if (env.IsDevelopment())
             {
@@ -43,6 +46,8 @@ namespace UMS
 
             //app.UseFileServer(fileServerOptions);
             app.UseStaticFiles();
+
+            Seeder.SeedIt(ctx, roleMgr, userMgr).Wait();
 
             app.UseMvcWithDefaultRoute();
 
